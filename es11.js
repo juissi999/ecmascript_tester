178dd8218118e236e5ef39a216a1d6a401df17b9
juissi999 <jussi.kaatiala@gmail.com>
2024-06-11 21:22:24 +0300

    Change es11-test to use eval because of syntax error possibility

--- es11.js
+++ es11.js
@@ -1,7 +1,9 @@
 es11 = document.getElementById('es11')
 function test_es11(elId) {
   try {
-    nullishCoalescingOperator = undefined ?? 'string'
+    possibly_syntax_error_code = 'nullishCoalescingOperator = undefined ?? 1'
+
+    eval(possibly_syntax_error_code)
 
     elId.innerHTML = 'es 11 works!'
   } catch (error) {
